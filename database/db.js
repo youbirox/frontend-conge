3fdcae4ad7eb8bcb610715c8057929b36fb1cfa0
youbirox <ayoubmoutii1994@gmail.com>
2020-12-15 17:08:28 +0100

    make it better

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 const db = {}
-const sequelize = new Sequelize('gestion_gonge','root','',{
-        host : '192.168.1.41',
+const sequelize = new Sequelize('heroku_fd10588392a82f1','bafbc53efdba3f','8b6c334a',{
+        host : 'us-cdbr-east-02.cleardb.com',
         dialect : 'mysql',
         operatorsAliases:false,
 
